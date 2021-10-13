bc2709997b2ed9d533c8b72bf31c4004821f9d4f
Amin Osman <osman.182@osu.edu>
2021-10-13 00:06:51 -0400

    Added loader

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const WebpackNotifierPlugin = require("webpack-notifier");
+const baseConfig = {
+  context: `${__dirname}`,
+  entry: {
+    batchreview: ["babel-polyfill", "./src/App.tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts", "css"],
+  },
+  mode: "development",
+  watch: true,
+  devtool: "source-map",
+  plugins: [
+    new WebpackNotifierPlugin({
+      alwaysNotify: true,
+      title: "Mars Batch Review",
+    }),
+  ],
+  // Great optimization for later
+  // externals: {
+  // 	"react": "React",
+  // 	"react-dom": "ReactDOM"
+  // }
+};
+
+const recipeConfig = Object.assign({}, baseConfig, {
+  output: {
+    filename: "[name].bundle.js",
+    path: `${__dirname}/../web-app/js`,
+  },
+});
+
+// const clientConfig = Object.assign({}, baseConfig, {
+// 	output: {
+// 		filename: '[name].bundle.js',
+// 		path: `${__dirname}/../../../Client/Scripts`,
+// 	},
+// })
+
+module.exports = [recipeConfig];
