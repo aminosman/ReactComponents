555d1f85d7b0bf4f2e3a8160c75d559e40529236
Amin Osman <osman.182@osu.edu>
2021-12-06 22:27:32 -0500

    Attempt to fix import

--- src/Table.tsx
+++ src/Table.tsx
@@ -12,7 +12,7 @@ import { Alert, Row } from 'react-bootstrap'
 import ContentLoader from 'react-content-loader'
 import { Link } from 'react-router-dom'
 import { DragDropContext, Droppable, Draggable, DraggableStateSnapshot } from 'react-beautiful-dnd';
-import { ItemEditSchema, ItemOptions, ItemSchema, Options, Props, TableCellProps } from '../index'
+import { ItemEditSchema, ItemOptions, ItemSchema, Options, Props, TableCellProps } from '../index.d.ts'
 
 
 const TableCell = ({ snapshot, children, Wrapper, row, id, ...props }: TableCellProps) => {
