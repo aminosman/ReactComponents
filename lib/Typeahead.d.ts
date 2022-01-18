93353d455ccbf2d0a1578968d02ced75e12a31ea
Amin Osman <osman.182@osu.edu>
2022-01-17 23:07:27 -0500

    Added support for passing in typeahead options directly

--- lib/Typeahead.d.ts
+++ lib/Typeahead.d.ts
@@ -1,12 +1,13 @@
-/// <reference types="react" />
+import { Option } from 'react-bootstrap-typeahead/types/types';
 export interface TypeaheadProps<T> {
     onChange: (item: T) => void;
     initialValue?: string;
-    onSearch: (term: string) => Promise<Array<{
+    onSearch?: (term: string) => Promise<Array<{
         label: string;
     }>>;
     searchOnClick?: boolean;
     onInputChange?: (term: string) => void;
+    options?: Option[];
 }
 declare const _default: <T extends object>(props: TypeaheadProps<T>) => JSX.Element;
 export default _default;
