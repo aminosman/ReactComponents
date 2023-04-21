6a1539ff2373201d6761c8030259f1ffc72a84d0
Amin Osman <osman.182@osu.edu>
2023-04-21 00:50:32 -0400

    Remove changes

--- src/Table.tsx
+++ src/Table.tsx
@@ -346,7 +346,7 @@ const TableLoader = <T extends object>(props: TableProps<T>) => {
             case 'number':
                 return (
                     <Form.Group as={Col} controlId={`${item.property as string}`} key={`form-info-${String(item.key || item.property)}-${type}`}>
-                        <Form.Label className="text-white">{item.label}{`${!!`${editingField.value}`.trim()}`}</Form.Label>
+                        <Form.Label className="text-white">{item.label}</Form.Label>
                         <InputGroup>
                             <Form.Control
                                 required={item.required}
@@ -355,7 +355,6 @@ const TableLoader = <T extends object>(props: TableProps<T>) => {
                                     : `${editingField.value}` : ''}
                                 onChange={(e: any) => onEditValueChange(item.key || item.property, e.target.value)}
                                 disabled={item.editable === false}
-                                isInvalid={!`${editingField.value}`.trim()}
                             />
                             {renderUnits(item?.units?.(editingField.item) || '')}
                         </InputGroup>
